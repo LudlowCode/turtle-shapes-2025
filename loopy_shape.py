74bcd52270eced3a095850bb6a1381c3a747da5c
D Edwards <dedwards@thequeensschool.co.uk>
2024-09-30 12:25:31 +0100

    star works

--- loopy_shape.py
+++ loopy_shape.py
@@ -1,8 +1,11 @@
 from turtle import *
 start_pos = pos()
+points = 18
+counter = 0
 angle = 100
-while True:
+while counter < points:
     forward(100)
     right(angle) 
     if pos() == start_pos:
         break
+    counter += 1
